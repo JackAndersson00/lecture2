2478172679fdadda3f62b9c35ad0c5a5f37170e4
Jack Andersson <jack.andersson@student.kyh.se>
2021-09-16 13:23:43 +0200

    invalid code?

--- Project/src/com/company/Main.java
+++ Project/src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hey there buddy!");
         System.out.println("Hahahaha!");
+
+        System.out.println("fellaktigt kommando");
     }
 }
